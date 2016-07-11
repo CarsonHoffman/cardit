b503e5e13475bdc769acd8093dbf321637fef988
Carson Hoffman <carsonhoffman17@yahoo.com>
2016-07-10 20:58:38 -0400

    Initial Commit

---
+++ Source/Cardit/CarditGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "CarditGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARDIT_API ACarditGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
